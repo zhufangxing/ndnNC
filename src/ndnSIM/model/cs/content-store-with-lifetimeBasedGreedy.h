7531c6fdc7ac3b7e2bf69667b36dff831c218b56
zfx <zfxrj@163.com>
2015-08-09 19:56:24 +0800

    add lifetimeBasedGreedy-cache-strategy

--- src/ndnSIM/model/cs/content-store-with-lifetimeBasedGreedy.h
+++ src/ndnSIM/model/cs/content-store-with-lifetimeBasedGreedy.h
@@ -24,18 +24,188 @@
 #include "content-store-impl.h"
 
 #include "../../utils/trie/multi-policy.h"
-#include "custom-policies/freshness-policy.h"
 
+#include <boost/intrusive/options.hpp>
+#include <boost/intrusive/list.hpp>
+
+#include <ns3/nstime.h>
+#include <ns3/simulator.h>
+#include <ns3/traced-callback.h>
+
+namespace ns3 {
+namespace ndn {
+namespace ndnSIM{
+
+
+// add by zfx
+// This file is modified by following two files: custom_policies/freshness_police.h  and content-store-with-freshness.h
+// LifetimeBaseGreedy_policy_traits : modified from custom_policies/freshness_police.h 
+// modify lookup and update functions.  
+
+/**
+ * @brief Traits for freshness policy
+ */
+struct mLifetimeBaseGreedy_policy_traits
+{
+  /// @brief Name that can be used to identify the policy (for NS-3 object model and logging)
+  static std::string GetName () { return "Freshness"; }
+
+  struct policy_hook_type : public boost::intrusive::set_member_hook<> { Time timeWhenShouldExpire; };
+
+  template<class Container>
+  struct container_hook
+  {
+    typedef boost::intrusive::member_hook< Container,
+                                           policy_hook_type,
+                                           &Container::policy_hook_ > type;
+  };
+
+  template<class Base,
+           class Container,
+           class Hook>
+  struct policy
+  {
+    static Time& get_freshness (typename Container::iterator item)
+    {
+      return static_cast<typename policy_container::value_traits::hook_type*>
+        (policy_container::value_traits::to_node_ptr(*item))->timeWhenShouldExpire;
+    }
+
+    static const Time& get_freshness (typename Container::const_iterator item)
+    {
+      return static_cast<const typename policy_container::value_traits::hook_type*>
+        (policy_container::value_traits::to_node_ptr(*item))->timeWhenShouldExpire;
+    }
+
+    template<class Key>
+    struct MemberHookLess
+    {
+      bool operator () (const Key &a, const Key &b) const
+      {
+        return get_freshness (&a) < get_freshness (&b);
+      }
+    };
+
+    typedef boost::intrusive::multiset< Container,
+                                   boost::intrusive::compare< MemberHookLess< Container > >,
+                                   Hook > policy_container;
+
+
+    class type : public policy_container
+    {
+    public:
+      typedef policy policy_base; // to get access to get_freshness methods from outside
+      typedef Container parent_trie;
+
+      type (Base &base)
+        : base_ (base)
+        , max_size_ (100)
+      {
+      }
+
+      inline void
+      update (typename parent_trie::iterator item)
+      {
+        // do nothing
+        Time freshness = item->payload ()->GetHeader ()->GetFreshness ();
+        if (!freshness.IsZero ())
+          {
+            get_freshness (item) = Simulator::Now () + freshness;
+
+            // push item only if freshness is non zero. otherwise, this payload is not controlled by the policy
+            // note that .size() on this policy would return only number of items with non-infinite freshness policy
+            policy_container::push_back (*item);
+          }
+      }
+
+      inline bool
+      insert (typename parent_trie::iterator item)
+      {
+        // get_time (item) = Simulator::Now ();
+        Time freshness = item->payload ()->GetHeader ()->GetFreshness ();
+        if (!freshness.IsZero ())
+          {
+            get_freshness (item) = Simulator::Now () + freshness;
+
+            // push item only if freshness is non zero. otherwise, this payload is not controlled by the policy
+            // note that .size() on this policy would return only number of items with non-infinite freshness policy
+            policy_container::push_back (*item);
+          }
+
+        return true;
+      }
+
+      inline void
+      lookup (typename parent_trie::iterator item)
+      {
+        Time freshness = item->payload ()->GetHeader ()->GetFreshness ();
+        if (!freshness.IsZero ())
+          {
+            get_freshness (item) = Simulator::Now () + freshness;
+
+            // push item only if freshness is non zero. otherwise, this payload is not controlled by the policy
+            // note that .size() on this policy would return only number of items with non-infinite freshness policy
+            policy_container::push_back (*item);
+          }
+      }
+
+      inline void
+      erase (typename parent_trie::iterator item)
+      {
+        if (!item->payload ()->GetHeader ()->GetFreshness ().IsZero ())
+          {
+            // erase only if freshness is non zero (otherwise an item is not in the policy
+            policy_container::erase (policy_container::s_iterator_to (*item));
+          }
+      }
+
+      inline void
+      clear ()
+      {
+        policy_container::clear ();
+      }
+
+      inline void
+      set_max_size (size_t max_size)
+      {
+        max_size_ = max_size;
+      }
+
+      inline size_t
+      get_max_size () const
+      {
+        return max_size_;
+      }
+
+    private:
+      type () : base_(*((Base*)0)) { };
+
+    private:
+      Base &base_;
+      size_t max_size_;
+    };
+  };
+};
+} // namespace ndnSIM
+} // namespace ndn
+} // namespace ns3
+
+
+
+
+// zfx
+// modified from content-store-with-freshness.h
 namespace ns3 {
 namespace ndn {
 namespace cs {
 
+
 template<class Policy>
 class ContentStoreWithLifetimeBasedGreedy :
-    public ContentStoreImpl< ndnSIM::multi_policy_traits< boost::mpl::vector2< Policy, ndnSIM::freshness_policy_traits > > >
+    public ContentStoreImpl< ndnSIM::multi_policy_traits< boost::mpl::vector2< Policy, ndnSIM::mLifetimeBaseGreedy_policy_traits > > >
 {
 public:
-  typedef ContentStoreImpl< ndnSIM::multi_policy_traits< boost::mpl::vector2< Policy, ndnSIM::freshness_policy_traits > > > super;
+  typedef ContentStoreImpl< ndnSIM::multi_policy_traits< boost::mpl::vector2< Policy, ndnSIM::mLifetimeBaseGreedy_policy_traits > > > super;
 
   typedef typename super::policy_container::template index<1>::type freshness_policy_container;
 
@@ -96,7 +266,7 @@ ContentStoreWithLifetimeBasedGreedy< Policy >::Add (Ptr<const ContentObject> hea
     {
       CleanExpired ();
     }
-  if (!ContentStoreImpl::isFull())
+  if (!super::isFull())
   {
     bool ok = super::Add (header, packet);
     if (!ok) return false;
